5e58ed64af111758c6fe35c2affd511771af19e9
Vicky S. <112182245+vickyviccc@users.noreply.github.com>
2024-10-07 06:16:33 +0200

    Added dictionary api integration

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+ <main>
+  <Dictionary />
+ </main>
+<footer>
+  <small>Open-source code by Vicky Stutter</small> 
+</footer>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "ob013544229b2c6b3ft9f43ac4050481";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Look for a word"
+        />
+      </form>
+    </div>
+  );
+}
